c47e09a13d9bc9305d622af8d0e2a57140a27355
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-27 23:06:58 +0100

    Simvix

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Página de inicio
+// Este archivo muestra un mensaje en la página principal
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página de inicio</title>
+</head>
+<body>
+    <p>Hola Jovi, estoy programando en asp.net core sin tener ni idea de como funciona</p>
+</body>
+</html>
